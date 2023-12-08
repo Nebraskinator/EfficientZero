18580ab4f1ea8c7e69f2911d64071e7fb6b2df89
Nebraskinator <ruhe@ucsb.edu>
2023-12-07 21:33:22 -0800

    colab compat

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 from core.test import test
 from core.train import train
 from core.utils import init_logger, make_results_dir, set_seed
-print("cuda available: {torch.cuda.is_available()}")
+print("cuda available: {}".format(torch.cuda.is_available()))
 
 if __name__ == '__main__':
     # Lets gather arguments
